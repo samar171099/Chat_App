2b97d0ecb2dba4789ef12acf953b40806006b958
Samar Saini <akki123samar@gmail.com>
2020-10-01 02:13:13 +0530

    chatapp

--- app/src/main/java/com/example/chatapp/fragment/PeopleFragment.kt
+++ app/src/main/java/com/example/chatapp/fragment/PeopleFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chatapp.AppConstants
 import com.example.chatapp.ChatActivity
 import com.example.chatapp.R
@@ -16,8 +15,8 @@ import com.google.firebase.firestore.ListenerRegistration
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.OnItemClickListener
 import com.xwray.groupie.Section
-import com.xwray.groupie.ViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.fragment_people.*
 import org.jetbrains.anko.support.v4.startActivity
 
@@ -26,7 +25,6 @@ class PeopleFragment : Fragment() {
 
 
     private lateinit var userListenerRegistration : ListenerRegistration
-
 
 
     private var shouldInitRecylerView = true
@@ -39,8 +37,14 @@ class PeopleFragment : Fragment() {
         userListenerRegistration =
             FirestoreUtil.addUsersListener(this.requireActivity(), this::updateRecyclerView)
 
+
         return inflater.inflate(R.layout.fragment_people, container, false)
     }
+
+
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -51,15 +55,23 @@ class PeopleFragment : Fragment() {
     private fun updateRecyclerView(items: List<Item>) {
 
         fun init() {
+
             recycler_view_people.apply {
-                layoutManager = LinearLayoutManager(this@PeopleFragment.context)
 
                 adapter = GroupAdapter<ViewHolder>().apply {
                     peopleSection = Section(items)
                     add(peopleSection)
-                    setOnItemClickListener(onItemClick)
+//                    setOnItemClickListener { item, view ->
+//                        if (item is PersonItem) {
+//                        startActivity<ChatActivity>(
+//                            AppConstants.USER_NAME to item.person.name,
+//                            AppConstants.USER_ID to item.userId
+//                        )
+//                    }
+//                    }
                 }
             }
+
             shouldInitRecylerView = false
         }
 
@@ -83,5 +95,7 @@ class PeopleFragment : Fragment() {
         }
     }
 
-
 }
+
+
+
